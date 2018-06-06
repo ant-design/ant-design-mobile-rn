79a77032a2d6500f4498f060a6c190e59efb5609
然则 <hualei.hl@alibaba-inc.com>
2018-06-06 10:48:25 +0800

    init: fix pub error

---
+++ index.js
@@ -0,0 +1,2 @@
+// antd-tools 只是为 web 项目构建、并生成 dist 目录，需要根目录下的 index.js 和 webpack.config.js
+// 但 RN 项目不需要，此处放个空文件 来利用 antd-tools 提供的 babel编译 / 打tag 等构建功能
---
+++ webpack.config.js
@@ -0,0 +1,4 @@
+const getWebpackConfig = require('antd-tools/lib/getWebpackConfig');
+const webpackConfig = getWebpackConfig(false);
+
+module.exports = webpackConfig;
