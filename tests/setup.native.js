8d137ad2fe7a0d8e2cd184de21957d92e3733b4b
bang <sqibang@gmail.com>
2018-10-20 20:44:27 +0800

    tests: fixes tests by upgrade jest

--- tests/setup.native.js
+++ tests/setup.native.js
@@ -3,7 +3,7 @@ const Enzyme = require('enzyme');
 const Adapter = require('enzyme-adapter-react-16');
 
 Enzyme.configure({ adapter: new Adapter() });
-
+jest.useFakeTimers();
 jest.mock('Linking', () => ({
   addEventListener: jest.fn(),
   removeEventListener: jest.fn(),
