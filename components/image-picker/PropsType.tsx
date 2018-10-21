b7915d665ba38dbd8336ed5d9c3f2b685e59909d
Gianfrancø Palumbo <gianpaj@users.noreply.github.com>
2018-10-21 14:34:58 +0300

    Fix #73 - ImagePicker prop types
    - update props "styles"
    - remove unused props

--- components/image-picker/PropsType.tsx
+++ components/image-picker/PropsType.tsx
@@ -1,13 +1,11 @@
 // import React from 'react';
 
 export interface ImagePickerPropTypes {
-  style?: {};
+  styles?: {};
   files?: Array<{}>;
   onChange?: (files: Array<{}>, operationType: string, index?: number) => void;
   onImageClick?: (index?: number, files?: Array<{}>) => void;
   onAddImageClick?: () => void;
   onFail?: (msg: string) => void;
   selectable?: boolean;
-  multiple?: boolean;
-  accept?: string;
 }
