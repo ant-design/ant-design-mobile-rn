f69dd78ead30634aaeaa10e5d49db73a9cfed5b1
bang <sqibang@gmail.com>
2018-11-04 22:19:26 +0800

    doc: codesandbox

--- components/button/demo/basic.md
+++ components/button/demo/basic.md
@@ -8,5 +8,56 @@ title:
 [Demo Source Code](https://github.com/ant-design/ant-design-mobile-rn/blob/master/components/button/demo/basic.native.tsx)
 
 ````jsx
-// todos: include('./basic.native.tsx')
+/* tslint:disable:no-unused-variable */
+// tslint:disable:jsx-no-multiline-js
+import React from 'react';
+import { Button, WhiteSpace, WingBlank } from 'antd-mobile-rn';
+
+/* tslint:disable:no-console */
+export default () => (
+  <WingBlank>
+    <WhiteSpace />
+    <Button>default</Button>
+    <WhiteSpace />
+    <Button disabled>default disabled</Button>
+    <WhiteSpace />
+
+    <Button type="primary">primary</Button>
+    <WhiteSpace />
+    <Button type="primary" disabled>
+      primary disabled
+    </Button>
+    <WhiteSpace />
+
+    <Button type="warning">warning</Button>
+    <WhiteSpace />
+    <Button type="warning" disabled>
+      warning disabled
+    </Button>
+    <WhiteSpace />
+
+    <Button loading>loading button</Button>
+
+    <Button activeStyle={false}>无点击反馈</Button><WhiteSpace />
+    <Button activeStyle={{ backgroundColor: 'red' }}>custom feedback style</Button><WhiteSpace />
+
+    <WingBlank
+      style={{
+        marginTop: 20,
+        flexDirection: 'row',
+        justifyContent: 'space-between',
+        alignItems: 'center',
+      }}
+    >
+      <Button type="ghost">ghost</Button>
+      <Button type="ghost" disabled>
+        ghost disabled
+      </Button>
+      <Button type="ghost" size="small">
+        ghost
+      </Button>
+    </WingBlank>
+  </WingBlank>
+);
+
 ````
--- site/desktop/src/template/Content/Demo.jsx
+++ site/desktop/src/template/Content/Demo.jsx
@@ -5,6 +5,7 @@ import classNames from 'classnames';
 import CopyToClipboard from 'react-copy-to-clipboard';
 import { FormattedMessage } from 'react-intl';
 import { Button, Modal, Radio, Tooltip, Icon } from 'antd';
+import { getParameters } from 'codesandbox/lib/api/define';
 import { ping } from '../../../../utils';
 
 export default class Demo extends React.Component {
@@ -87,6 +88,120 @@ export default class Demo extends React.Component {
 
   handleProgrammingLangChange = (e) => {
     this.setState({ lang: e.target.value });
+  }
+  renderCodeSandbox = () => {
+    const { sourceCode } = this.state;
+    const parameters = getParameters({
+      files: {
+        'public/index.html': {
+          content: `
+<!DOCTYPE html>
+<html lang="en">
+  <head>
+    <meta charset="utf-8">
+    <link rel="shortcut icon" href="%PUBLIC_URL%/favicon.ico">
+    <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
+    <meta name="theme-color" content="#000000">
+    <!--
+      manifest.json provides metadata used when your web app is added to the
+      homescreen on Android. See https://developers.google.com/web/fundamentals/web-app-manifest/
+    -->
+    <link rel="manifest" href="%PUBLIC_URL%/manifest.json">
+    <!--
+      Notice the use of %PUBLIC_URL% in the tags above.
+      It will be replaced with the URL of the public folder during the build.
+      Only files inside the public folder can be referenced from the HTML.
+
+      Unlike "/favicon.ico" or "favicon.ico", "%PUBLIC_URL%/favicon.ico" will
+      work correctly both with client-side routing and a non-root public URL.
+      Learn how to configure a non-root public URL by running npm run build.
+    -->
+    <title>React App</title>
+  </head>
+  <body>
+    <noscript>
+      You need to enable JavaScript to run this app.
+    </noscript>
+    <div id="root"></div>
+  </body>
+</html>
+`,
+        },
+        'src/index.js': {
+          content: `
+import { AppRegistry } from "react-native";
+import App from "./App";
+
+AppRegistry.registerComponent("App", () => App);
+
+AppRegistry.runApplication("App", {
+  rootTag: document.getElementById("root")
+});
+`,
+        },
+        'src/App.js': {
+          content: sourceCode,
+        },
+        //         'tsconfig.json':{
+        //           content: `
+        // {
+        //   "compilerOptions": {
+        //     "target": "es5",
+        //     "allowJs": true,
+        //     "skipLibCheck": true,
+        //     "esModuleInterop": true,
+        //     "allowSyntheticDefaultImports": true,
+        //     "strict": true,
+        //     "forceConsistentCasingInFileNames": true,
+        //     "module": "esnext",
+        //     "moduleResolution": "node",
+        //     "resolveJsonModule": true,
+        //     "isolatedModules": true,
+        //     "noEmit": true,
+        //     "jsx": "preserve",
+        //     "noImplicitAny": false
+        //   },
+        //   "include": [
+        //     "src"
+        //   ]
+        // }
+        // `
+        //         },
+        'package.json': {
+          content: {
+            version: '0.2.0',
+            scripts: {
+              test: 'react-scripts test',
+              start: 'react-scripts start',
+              eject: 'react-scripts eject',
+              build: 'react-scripts build',
+            },
+            private: true,
+            name: 'antd-rn-example',
+            eslintConfig: {
+              extends: 'react-app',
+            },
+            dependencies: {
+              'antd-mobile-rn': '^2.3.2-2',
+              'antd-mobile-rn-scripts': '2.1.5',
+              react: '^16.6.0',
+              'react-art': '^16.6.0',
+              'react-dom': '^16.6.0',
+              'react-native-web': '^0.9.6',
+            },
+            browserslist: [
+              '>0.2%',
+              'not dead',
+              'not ie <= 11',
+              'not op_mini all',
+            ],
+          },
+        },
+      },
+    });
+
+    const url = `https://codesandbox.io/api/v1/sandboxes/define?parameters=${parameters}&embed=1`;
+    return <iframe src={`${url}`} title="antd-mobile-rn-examles" style={{ width: '100%', height: 700, border: 0, borderRadius: 4, overflow: 'hidden' }} sandbox="allow-modals allow-forms allow-popups allow-scripts allow-same-origin" />;
   }
   /* eslint-disable react/jsx-indent */
   renderDemoCode = (highlightedCode, inModal) => {
@@ -273,6 +388,7 @@ export default class Demo extends React.Component {
           {this.renderDemoCode(highlightedCode, false)}
           {hsNode}
         </section>
+        {this.renderCodeSandbox(highlightedCode)}
       </section>
     );
   }
