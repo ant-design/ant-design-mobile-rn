56a99200ad37352bcf39790729847c819ed3faa0
bang <sqibang@gmail.com>
2018-11-16 16:27:05 +0800

    feat: exports props wip

--- components/locale-provider/index.tsx
+++ components/locale-provider/index.tsx
@@ -2,12 +2,29 @@ import en_US from './en_US';
 import React, { useContext } from 'react';
 import deepmerge from 'deepmerge';
 export const LocaleContext = React.createContext(en_US);
-// TODO: ts
-export const LocaleProvider = (props: any) => (
-  <LocaleContext.Provider {...props} />
-);
+export interface Locale {
+  DatePicker: any;
+  DatePickerView: any;
+  InputItem: any;
+  Modal: any;
+  Pagination: any;
+  Picker: any;
+  SearchBar: any;
+}
 
-export const useLocale = (locale: any = {}) => {
+export type LocaleValue = Locale & { locale: string };
+export interface LocaleProviderProps {
+  value: Locale & { locale: string };
+  children?: React.ReactNode;
+}
+
+export const LocaleProvider = (props: LocaleProviderProps) => {
+  return <LocaleContext.Provider {...props} />;
+};
+
+export const useLocale = (locale: Partial<Locale> = {}) => {
   const l = useContext(LocaleContext);
   return deepmerge(l, locale);
 };
+
+export default LocaleProvider;
