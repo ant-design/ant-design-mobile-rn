5c1db35fd3d0c60e472f556ebf0cc1534b75354d
bang <sqibang@gmail.com>
2018-12-04 11:09:36 +0800

    refactor(swipe-action): use react-native-swipeout directly

--- components/swipe-action/index.tsx
+++ components/swipe-action/index.tsx
@@ -1,16 +1,90 @@
-import Swipeout from 'rc-swipeout/lib/Swipeout';
 import React from 'react';
-import { StyleProp, TextStyle, ViewStyle } from 'react-native';
-import { SwipeActionPropsType } from './PropsType';
+import { StyleProp, Text, TextStyle, View } from 'react-native';
+import Swipeout, { SwipeoutButtonProperties, SwipeoutProperties } from 'react-native-swipeout';
 
-export interface SwipeActionProps extends SwipeActionPropsType<TextStyle> {
-  styles?: any;
-  style?: StyleProp<ViewStyle>;
+export interface SwipeActionProps extends SwipeoutProperties {}
+export interface SwipeoutButtonProps extends SwipeoutButtonProperties {
+  style?: StyleProp<TextStyle> & { backgroundColor: string };
 }
-
-class SwipeAction extends React.Component<SwipeActionProps, any> {
+class SwipeAction extends React.Component<SwipeActionProps> {
+  static defaultProps: SwipeActionProps = {
+    autoClose: false,
+    disabled: false,
+    onOpen() {},
+    onClose() {},
+  };
+  renderCustomButton(button: SwipeoutButtonProps) {
+    const buttonStyle = button.style;
+    const bgColor = buttonStyle ? buttonStyle.backgroundColor : 'transparent';
+    const Component = (
+      <View
+        style={{
+          flex: 1,
+          justifyContent: 'center',
+          alignItems: 'center',
+          backgroundColor: bgColor,
+        }}
+      >
+        {React.isValidElement(button.text) ? (
+          button.text
+        ) : (
+          <Text style={[buttonStyle, { textAlign: 'center' }]}>
+            {button.text}
+          </Text>
+        )}
+      </View>
+    );
+    return {
+      text: button.text || 'Click',
+      onPress: button.onPress,
+      type: 'default',
+      component: Component,
+      backgroundColor: 'transparent',
+      color: '#999',
+      disabled: false,
+    };
+  }
   render() {
-    return <Swipeout {...this.props} />;
+    const {
+      disabled,
+      autoClose,
+      style,
+      left,
+      right,
+      onOpen,
+      onClose,
+      children,
+      ...restProps
+    } = this.props;
+
+    const customLeft =
+      left &&
+      left.map(btn => {
+        return this.renderCustomButton(btn);
+      });
+    const customRight =
+      right &&
+      right.map(btn => {
+        return this.renderCustomButton(btn);
+      });
+
+    return customLeft || customRight ? (
+      <Swipeout
+        autoClose={autoClose}
+        left={customLeft as SwipeoutButtonProps[]}
+        right={customRight as SwipeoutButtonProps[]}
+        style={style}
+        onOpen={onOpen}
+        onClose={onClose}
+        disabled={disabled}
+      >
+        {children}
+      </Swipeout>
+    ) : (
+      <View style={style} {...restProps}>
+        {children}
+      </View>
+    );
   }
 }
 
