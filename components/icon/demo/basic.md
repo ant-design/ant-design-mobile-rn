6485162d992c9e45a9ecfbb345f7e1f04eede04e
bang <sqibang@gmail.com>
2018-12-05 14:13:09 +0800

    chore: site

--- components/icon/demo/basic.md
+++ components/icon/demo/basic.md
@@ -7,6 +7,27 @@ title:
 
 [Demo Source Code](https://github.com/ant-design/ant-design-mobile-rn/blob/master/components/icon/demo/basic.tsx)
 
-````jsx
-// todos: include('./basic.tsx')
-````
+```jsx
+import React from 'react';
+import { Grid, Icon } from '../../';
+import { ScrollView } from 'react-native';
+import { outlineGlyphMap } from '@ant-design/icons-react-native/lib/outline';
+
+export default class IConDemo extends React.Component {
+  render() {
+    const outlineData = Object.keys(outlineGlyphMap).map(item => ({
+      icon: <Icon name={item} />,
+      text: item,
+    }));
+
+    return (
+      <ScrollView>
+        <Grid data={outlineData} columnNum={3} hasLine={false} />
+      </ScrollView>
+    );
+  }
+}
+
+export const title = 'Icon';
+export const description = 'Icon Example';
+```
---
+++ site/desktop/src/template/Content/Sandbox.jsx
@@ -0,0 +1,88 @@
+export default (sourceCode, doc) => {
+  const sandboxName = `${doc.meta.title}`;
+  const parameters = {
+    files: {
+      'public/index.html': {
+        content: `
+<!DOCTYPE html>
+<html lang="en">
+<head>
+  <meta charset="utf-8">
+  <link rel="shortcut icon" href="%PUBLIC_URL%/favicon.ico">
+  <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
+  <meta name="theme-color" content="#000000">
+  <!--
+    manifest.json provides metadata used when your web app is added to the
+    homescreen on Android. See https://developers.google.com/web/fundamentals/web-app-manifest/
+  -->
+  <link rel="manifest" href="%PUBLIC_URL%/manifest.json">
+  <!--
+    Notice the use of %PUBLIC_URL% in the tags above.
+    It will be replaced with the URL of the public folder during the build.
+    Only files inside the public folder can be referenced from the HTML.
+
+    Unlike "/favicon.ico" or "favicon.ico", "%PUBLIC_URL%/favicon.ico" will
+    work correctly both with client-side routing and a non-root public URL.
+    Learn how to configure a non-root public URL by running npm run build.
+  -->
+  <title>React App</title>
+</head>
+<body>
+  <noscript>
+    You need to enable JavaScript to run this app.
+  </noscript>
+  <div id="root"></div>
+</body>
+</html>
+`,
+      },
+      'src/index.js': {
+        content: `
+
+import { AppRegistry } from "react-native";
+import App from "./App";
+
+AppRegistry.registerComponent("App", () => App);
+
+AppRegistry.runApplication("App", {
+rootTag: document.getElementById("root")
+});
+`,
+      },
+      'src/App.js': {
+        content: sourceCode,
+      },
+      'package.json': {
+        content: {
+          version: '0.2.0',
+          scripts: {
+            test: 'react-scripts test',
+            start: 'react-scripts start',
+            eject: 'react-scripts eject',
+            build: 'react-scripts build',
+          },
+          private: true,
+          name: sandboxName,
+          eslintConfig: {
+            extends: 'react-app',
+          },
+          dependencies: {
+            '@ant-design/react-native': 'next',
+            'antd-mobile-rn-scripts': 'latest',
+            react: '^16.6.0',
+            'react-art': '^16.6.0',
+            'react-dom': '^16.6.0',
+            'react-native-web': '^0.9.6',
+          },
+          browserslist: [
+            '>0.2%',
+            'not dead',
+            'not ie <= 11',
+            'not op_mini all',
+          ],
+        },
+      },
+    },
+  };
+  return parameters;
+};
