59483d01108116e28cbc0a1ae8e9f527b80f4627
bang <sqibang@gmail.com>
2018-12-15 22:09:51 +0800

    fix: jest

--- .jest.config.js
+++ .jest.config.js
@@ -4,7 +4,7 @@ const {defaults: tsjPreset} = require('ts-jest/presets');
 const transformPackages = [
   'react-native', 'react-native-menu', 'react-native-tab-view',
   'react-native-collapsible', 'react-native-swipeout',
-  'react-native-camera-roll-picker', 'react-native-ultimate-listview',
+  'react-native-camera-roll-picker', '@bang88/react-native-ultimate-listview',
   'react-native-safe-area-view'
 ]
 
