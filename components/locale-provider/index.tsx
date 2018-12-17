0885824ac48376392f3bad4e0891361f4224f557
bang <sqibang@gmail.com>
2018-12-17 09:46:52 +0800

    feat: add Locale definition

--- components/locale-provider/index.tsx
+++ components/locale-provider/index.tsx
@@ -1,15 +1,99 @@
 import PropTypes from 'prop-types';
 import React from 'react';
-
-export interface LocaleProviderProps {
-  locale: {
-    Pagination?: object;
-    DatePicker?: object;
-    DatePickerView?: object;
-    InputItem?: object;
-    Modal?: object;
-    ListView?: object;
+export interface Locale {
+  /** zh_CN */
+  locale: string;
+  DatePicker: {
+    /** 确定 */
+    okText: string;
+    /** 取消 */
+    dismissText: string;
+    /** 请选择 */
+    extra: string;
+    DatePickerLocale: {
+      /** 年 */
+      year: string;
+      /** 月 */
+      month: string;
+      /** 日 */
+      day: string;
+      /** 时 */
+      hour: string;
+      /** 分 */
+      minute: string;
+      /** 上午 */
+      am: string;
+      /** 下午 */
+      pm: string;
+    };
   };
+  DatePickerView: {
+    /** 年 */
+    year: string;
+    /** 月 */
+    month: string;
+    /** 日 */
+    day: string;
+    /** 时 */
+    hour: string;
+    /** 分 */
+    minute: string;
+    /** 上午 */
+    am: string;
+    /** 下午 */
+    pm: string;
+  };
+  InputItem: {
+    /** 确定 */
+    confirmLabel: string;
+    /** 退格 */
+    backspaceLabel: string;
+    /** 收起键盘 */
+    cancelKeyboardLabel: string;
+  };
+  Modal: {
+    /** 确定 */
+    okText: string;
+    /** 取消 */
+    cancelText: string;
+    /** 按钮 */
+    buttonText: string;
+  };
+  Pagination: {
+    /** 上一页 */
+    prevText: string;
+    /** 下一页 */
+    nextText: string;
+  };
+  Picker: {
+    /** 确定 */
+    okText: string;
+    /** 取消 */
+    dismissText: string;
+    /** 请选择 */
+    extra: string;
+  };
+  SearchBar: {
+    /** 取消 */
+    cancelText: string;
+  };
+  ListView: {
+    /** 已加载完 */
+    done: string;
+    /** 加载中... */
+    loading: string;
+    /** 下拉刷新 */
+    refreshableTitlePull: string;
+    /** 释放加载 */
+    refreshableTitleRelease: string;
+    /** 加载中... */
+    refreshableTitleRefreshing: string;
+    /** 暂无数据 */
+    noData: string;
+  };
+}
+export interface LocaleProviderProps {
+  locale?: Partial<Locale>;
   children?: React.ReactElement<any>;
 }
 
--- components/provider/index.tsx
+++ components/provider/index.tsx
@@ -1,9 +1,9 @@
 import * as React from 'react';
-import LocaleProvider from '../locale-provider';
+import LocaleProvider, { Locale } from '../locale-provider';
 import Portal from '../portal';
 import { Theme, ThemeProvider } from '../style';
 export interface ProviderProps {
-  locale?: any;
+  locale?: Partial<Locale>;
   theme?: Partial<Theme>;
 }
 export default class Provider extends React.Component<ProviderProps> {
