6077619382a64d4112a64222570ec11bcd2cb71b
bang <sqibang@gmail.com>
2019-09-27 11:43:43 +0800

    fix: locale

--- components/list-view/locale/en_US.tsx
+++ components/list-view/locale/en_US.tsx
@@ -1,6 +1,6 @@
 export default {
-  Done: 'Loaded',
-  Loading: 'Loading',
+  done: 'Loaded',
+  loading: 'Loading',
   refreshableTitlePull: 'Pull down refresh',
   refreshableTitleRelease: 'Release loading',
   refreshableTitleRefreshing: 'Loading...',
