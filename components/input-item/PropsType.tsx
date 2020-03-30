a0835af63e365cd3c5faa9ef63fadf4c87fd3d41
damon.chen <chj_0507_dz@sina.com>
2020-03-30 17:58:24 +0800

    修复InputItem的textAlign属性没有right值的问题 (#786)
    Co-authored-by: chj_damon <chjdamon@gmail.com>

--- components/input-item/PropsType.tsx
+++ components/input-item/PropsType.tsx
@@ -25,7 +25,7 @@ export interface InputItemPropsType {
   // size?: 'large' | 'small';
   labelNumber?: number;
   labelPosition?: 'left' | 'top';
-  textAlign?: 'left' | 'center';
+  textAlign?: 'left' | 'center' | 'right';
   updatePlaceholder?: boolean;
   locale?: object;
   onChange?: (value: string) => void;
