7be9f510248e88885cd6614d3614a4ca7dc9d568
罗坤 <1498710037@qq.com>
2020-04-22 13:01:53 +0800

    fix:@react-native-community/XXX jest error

--- components/picker/NativePicker.ios.tsx
+++ components/picker/NativePicker.ios.tsx
@@ -1,1 +1,1 @@
-export  { Picker as default } from 'react-native';
+export { Picker as default } from '@react-native-community/picker';
--- rn-kitchen-sink/ios/Pods/Target Support Files/Pods-KitchenSink-KitchenSinkTests/Pods-KitchenSink-KitchenSinkTests-dummy.m
+++
@@ -1,5 +0,0 @@
-#import <Foundation/Foundation.h>
-@interface PodsDummy_Pods_KitchenSink_KitchenSinkTests : NSObject
-@end
-@implementation PodsDummy_Pods_KitchenSink_KitchenSinkTests
-@end
