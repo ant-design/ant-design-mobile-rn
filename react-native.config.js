c49a22fa7c2d197eccfb3067c52ba653b6ab7c09
bang <sqibang@gmail.com>
2020-04-30 14:44:56 +0800

    fix: auto link

--- react-native.config.js
+++ react-native.config.js
@@ -1,10 +1,5 @@
 module.exports = {
   dependency: {
-    assets: ['./node_modules/@ant-design/icons-react-native/fonts'],
-  },
-  project: {
-    android: {
-      sourceDir: './rn-kitchen-sink/android',
-    },
+    assets: ['../icons-react-native/fonts'],
   },
 };
