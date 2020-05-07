ed9e014efe19c7dbab220071c2a98856df93d527
bang <sqibang@gmail.com>
2020-05-07 10:48:03 +0800

    feat: icon add search

--- components/icon/demo/basic.tsx
+++ components/icon/demo/basic.tsx
@@ -1,23 +1,27 @@
+import { outlineGlyphMap, OutlineGlyphMapType } from '@ant-design/icons-react-native/lib/outline';
 import React from 'react';
-import { Grid, Icon } from '../../';
 import { ScrollView } from 'react-native';
-import {
-  outlineGlyphMap,
-  OutlineGlyphMapType,
-} from '@ant-design/icons-react-native/lib/outline';
-
+import { Grid, Icon, SearchBar } from '../../';
+const data = Object.keys(outlineGlyphMap).map((item: OutlineGlyphMapType) => ({
+  icon: <Icon name={item} />,
+  text: item,
+}));
 export default class IConDemo extends React.Component<any, any> {
+  state = {
+    data,
+  };
   render() {
-    const outlineData = Object.keys(outlineGlyphMap).map(
-      (item: OutlineGlyphMapType) => ({
-        icon: <Icon name={item} />,
-        text: item,
-      }),
-    );
-
     return (
       <ScrollView>
-        <Grid data={outlineData} columnNum={3} hasLine={false} />
+        <SearchBar
+          placeholder="Search by icon name"
+          onChange={text => {
+            this.setState(() => ({
+              data: data.filter(d => d.text.match(new RegExp(text, 'gi'))),
+            }));
+          }}
+        />
+        <Grid data={this.state.data} columnNum={3} hasLine={false} />
       </ScrollView>
     );
   }
