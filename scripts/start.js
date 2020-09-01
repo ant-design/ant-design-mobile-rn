349952116d16bb4c9c1ed66ad29c5b3fb9039e7a
helsonxiao <helsonxiao@gmail.com>
2020-09-01 21:38:17 +0800

    fix: missing script dep

--- scripts/start.js
+++ scripts/start.js
@@ -5,7 +5,7 @@ function run() {
   exec.killPort(8081);
   exec.execSync('watchman watch-del-all || true');
   exec.execSync('adb reverse tcp:8081 tcp:8081 || true');
-  exec.execSync('node ./node_modules/react-native/local-cli/cli.js start');
+  exec.execSync('npx react-native start');
 }
 
 run();
