af3bd6326897fc5455ee6a2f015e2987e500e3ba
helsonxiao <helsonxiao@gmail.com>
2020-09-05 18:02:38 +0800

    fix: kill script

--- scripts/start.js
+++ scripts/start.js
@@ -2,7 +2,7 @@ const exec = require('shell-utils').exec;
 
 
 function run() {
-  exec.killPort(8081);
+  exec.execSync('lsof -i:8081 | grep \'node\' | awk \'{print $2}\' | xargs kill -9');
   exec.execSync('watchman watch-del-all || true');
   exec.execSync('adb reverse tcp:8081 tcp:8081 || true');
   exec.execSync('npx react-native start');
