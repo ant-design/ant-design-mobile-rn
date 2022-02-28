64a8a777e62427e2da27eb21e72866867075ad59
罗坤 <luokun199503@qq.com>
2022-02-28 18:36:14 +0800

    feat: expo support

--- babel.config.js
+++ babel.config.js
@@ -1,28 +1,15 @@
+const path = require('path')
 module.exports = {
-  env: {
-    development: {
-      presets: ['module:metro-react-native-babel-preset'],
-      plugins: [[
-        'import',
-        { libraryName: '@ant-design/react-native', libraryDirectory: 'components' },
-      ]],
-    },
-    production: {
-      presets: ['module:metro-react-native-babel-preset'],
-
-      plugins: [[
-        'import',
-        { libraryName: '@ant-design/react-native', libraryDirectory: 'components' },
-      ]],
-    },
-    test: {
-      presets: ['module:metro-react-native-babel-preset'],
-      plugins: [
-        [
-          'import',
-          { libraryName: '@ant-design/react-native', libraryDirectory: 'components' },
-        ],
-      ],
-    },
-  },
-};
+  presets: ['module:metro-react-native-babel-preset'],
+  plugins: [
+    [
+      'import',
+      {
+        libraryName: '@ant-design/react-native',
+        customName: (name) => {
+          return path.resolve(__dirname, `./components/${name}`)
+        },
+      },
+    ],
+  ],
+}
