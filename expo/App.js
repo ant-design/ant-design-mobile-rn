cfa5743a254be4d7f0dabb6967aea2b169d093f7
罗坤 <luokun199503@qq.com>
2022-03-03 18:56:55 +0800

    feat: expo Font

--- expo/App.js
+++ expo/App.js
@@ -1,4 +1,29 @@
-import { registerRootComponent } from 'expo'
+import AppLoading from 'expo-app-loading'
+import * as Font from 'expo-font'
+import React from 'react'
 import App from '../rn-kitchen-sink/App'
+export default class extends React.Component {
+  state = {
+    isReady: false,
+  }
 
-registerRootComponent(App)
+  async componentDidMount() {
+    await Font.loadAsync(
+      'antoutline',
+      require('@ant-design/icons-react-native/fonts/antoutline.ttf'),
+    )
+
+    await Font.loadAsync(
+      'antfill',
+      require('@ant-design/icons-react-native/fonts/antfill.ttf'),
+    )
+    this.setState({ isReady: true })
+  }
+  render() {
+    const { isReady } = this.state
+    if (!isReady) {
+      return <AppLoading />
+    }
+    return <App />
+  }
+}
