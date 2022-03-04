b199feb39b292daf35391827d7f1b52f3633d8a1
罗坤 <luokun199503@qq.com>
2022-03-04 17:10:30 +0800

    feat: webpack loader @bang88/react-native-ultimate-listview

--- webpack.config.js
+++ webpack.config.js
@@ -3,5 +3,22 @@ const createExpoWebpackConfigAsync = require('@expo/webpack-config')
 module.exports = async function (env, argv) {
   const config = await createExpoWebpackConfigAsync(env, argv)
   // Customize the config before returning it.
+  config.module.rules.push({
+    test: /\.(js)$/,
+    include: [/node_modules\/@bang88\/react-native-ultimate-listview/],
+    use: {
+      loader: 'babel-loader',
+      options: {
+        presets: [
+          [
+            '@babel/preset-env',
+            {
+              modules: false,
+            },
+          ],
+        ],
+      },
+    },
+  })
   return config
 }
