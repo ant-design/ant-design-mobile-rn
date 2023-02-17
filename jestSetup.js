d3e2253ef832175ad88003f644967e261b51ca13
罗坤 <luokun199503@qq.com>
2023-02-17 17:10:26 +0800

    fix: jest mock NativeModules

--- jestSetup.js
+++ jestSetup.js
@@ -1,3 +1,9 @@
-import 'react-native-gesture-handler/jestSetup';
+import 'react-native-gesture-handler/jestSetup'
 
-jest.mock('react-native/Libraries/Animated/NativeAnimatedHelper');
+jest.mock('react-native/Libraries/Animated/NativeAnimatedHelper')
+
+jest.mock('react-native', () => {
+  const RN = jest.requireActual('react-native')
+  RN.NativeModules.RNCCameraRollPermissionModule = {}
+  return RN
+})
