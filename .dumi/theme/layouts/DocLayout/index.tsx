403b8173bc39e6c181ae160a2f7d577db23b018e
罗坤 <luokun199503@qq.com>
2026-02-28 10:46:35 +0800

    fix: 首页不显示siderbar

--- .dumi/theme/layouts/DocLayout/index.tsx
+++ .dumi/theme/layouts/DocLayout/index.tsx
@@ -1,8 +1,18 @@
-import DefaultDocLayout from 'dumi/theme-default/layouts/DocLayout'
-import React from 'react'
-import './index.less'
+import { useLocation as useDumiLocation } from 'dumi';
+import DefaultDocLayout from 'dumi/theme-default/layouts/DocLayout';
+import React from 'react';
+import './index.less';
 
 export default function DocLayout(props: any) {
+  const location = useDumiLocation();
+  const { pathname } = location;
+  if (pathname === '/index-cn') {
+    return (
+      <div data-hide-sidebar="true" className="dumi-doc-layout-cn-index">
+        <DefaultDocLayout {...props} />
+      </div>
+    );
+  }
   return (
     <DefaultDocLayout {...props} />
   )
